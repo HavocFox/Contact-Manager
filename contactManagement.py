26f737b174d34006ed2019f3b99f8ae9783190d9
HavocFox <duskydraws@gmail.com>
2024-04-29 01:05:40 -0400

    needs delete and polishing

--- contactManagement.py
+++ contactManagement.py
@@ -1,3 +1,4 @@
+import re
 contacts = {
     '1234567890': {'Name': 'Mathew McConaughey', 'Email': 'crungus@gmail.com', 'Phone': '1234567890'},
     '2234567890': {'Name': '1Mathew McConaughey', 'Email': '1crungus@gmail.com', 'Phone': '2234567890'},
@@ -9,59 +10,48 @@ contacts = {
 #--------------------------------------------------------
 
 def contact_add():
+    duplicate = False
     while True:
         try:
-            iden_choice = int(input("Would you like to use a: \n1. Phone number\nor\n2. Email Address\nas your contact identifier? Please select a number. "))
+            while True:
+                identifier = input("What is the phone number you are entering contact information for? This will serve as an identifier: ")
+                if len(identifier) == 10:
+                    break
+                else:
+                    print("Please enter a valid 10-digit phone number (Without hyphens, including area code). ")
         except ValueError:
-            print("Please enter a valid selection. \n")
+            print("Please enter a valid phone number. ")
         else:
             break
 
-    if iden_choice == 1:
-        while True:
-            try:
-                identifier = int(input("What is the phone number you are entering contact information for? "))
-                if identifier > 1000000000:
-                    pass
-                else:
-                    print("You chose to enter a phone number. Please enter a valid phone number (Without hyphens, including area code). ")
-            except ValueError:
-                print("Please enter a valid phone number. ")
-            else:
-                break
+    for contact, info in contacts.items():
+        if identifier in info['Phone']:
+            print("You cannot enter a duplicate contact. ")
+            return
 
 
-        name = input("What is the name of this contact? ")
-        phone = identifier
-        email = input("What is their email address? ")
+    name = input("What is the name of this contact? ")
+    phone = identifier
 
+    pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
 
-    if iden_choice == 2:        # Potentially add in a search for an @ sign or a .com to make sure it is a valid address
-        while True:
-            try:
-                identifier = input("What is the email address you are entering contact information for? ")
-                if identifier.isSpace():
-                    print("Please enter a valid non-empty email.")
-            except:
-                print("Please enter a valid email. ")
-            else:
-                break
+    while True:
+        email = input("What is their email address? ").lower()
+        if not (re.match(pattern, email)):
+            print("Please enter a valid email. ")
 
+        for contact, info in contacts.items():
+            if email in info['Email']:
+                print("You cannot enter a duplicate contact. ")
+                duplicate = True
 
-        name = input("What is the name of this contact? ")
-        email = identifier
-        while True:
-            try:
-                phone = int(input("What is their phone number? "))
-                if phone > 1000000000:
-                    pass
-            except ValueError:
-                print("Please enter a valid phone number. ")
-
-            else:
-                break
+        if not duplicate:
+            break
+        duplicate = False
 
     contacts[identifier] = {"Name" : name, "Email" : email, "Phone" : phone}
+
+    print(f"The following contact has been added: {contacts[identifier]}\n")
 
 #--------------------------------------------------------
 
@@ -69,6 +59,50 @@ def contact_display():
     print("\nYour contacts: ")
     for contact, info in contacts.items():
         print(f'Identifier (Phone or Email): {contact} Name: {info['Name']} Email: {info['Email']} Phone #:  {info['Phone']}\n')
+
+#--------------------------------------------------------
+
+def contact_edit():
+    search_term = input("Which contact do you want to edit? Please enter its identifier. ")
+
+    for contact, info in contacts.items():
+        if search_term in contact:
+            print("This is the contact you are editing: ")
+            print(f'Identifier (Phone): {contact} Name: {info['Name']} Email: {info['Email']} Phone #: {info['Phone']}\n')
+
+    edit_choice = input("What part of the contact would you like to edit? Name, Email or Phone? ").lower()
+    if edit_choice == 'name':
+        new_value = input("Please enter the new name: ")
+        contacts[search_term]['Name'] = new_value
+        print(f"Name updated to {new_value}.\n")
+
+    if edit_choice == 'email':
+        pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
+        while True:
+            new_value = input("Please enter the new email: ")
+            if re.match(pattern, new_value):
+                contacts[search_term]['Email'] = new_value
+                print(f"Email updated to {new_value}.\n")
+                break
+            else:
+                print("Please enter a valid email. ")
+
+    if edit_choice == 'phone':
+        while True:
+            try:
+                new_value = int(input("Please enter the new phone number: "))
+                if new_value > 1000000000:
+                    break
+                else:
+                    print("Please enter a valid phone number.")
+            except ValueError:
+                print("Please enter a valid phone number.")
+            else:
+                break
+
+        contacts[new_value] = contacts.pop(search_term)
+        contacts[new_value]['Phone'] = new_value
+        print(f"Phone # updated to {new_value}.\n")
 
 #--------------------------------------------------------
 
@@ -80,7 +114,7 @@ def contact_search():
         if search_term in contact:
             print("Found! ")
             print("The contact that was found: ")
-            print(f'Identifier (Phone or Email): {contact} Name: {info['Name']} Email: {info['Email']} Phone #: {info['Phone']}\n')
+            print(f'Identifier (Phone): {contact} Name: {info['Name']} Email: {info['Email']} Phone #: {info['Phone']}\n')
 
 #--------------------------------------------------------
 
@@ -92,7 +126,7 @@ def contact_export():
 
             if ovchoice == 'Y' or not ovchoice:
                 for contact, info in contacts.items():
-                    file.write(f'Identifier (Phone or Email): {contact} Name: {info['Name']} Email: {info['Email']} Phone #: {info['Phone']}\n')
+                    file.write(f'Identifier): {contact} Name: {info['Name']} Email: {info['Email']} Phone #: {info['Phone']}\n')
                 print("success")
             else:
                 ("Returning to menu. \n")
@@ -131,7 +165,7 @@ user_choice = 0
 print("Welcome to the Contact Management System!")
 while True:
 
-    print('Menu: \n1. Add a new contact\n2. Edit an existing contact\n3. Delete a contact\n4. Search for a contact\n5. Display all contacts\n6. Export contacts to a text file\n7. Import contacts from a text file\n8. Quit')
+    print('\nMenu: \n1. Add a new contact\n2. Edit an existing contact\n3. Delete a contact\n4. Search for a contact\n5. Display all contacts\n6. Export contacts to a text file\n7. Import contacts from a text file\n8. Quit')
     try:
          user_choice = int(input("\nWhat would you like to do? Please enter the number of your selection.  "))
     except ValueError:
@@ -141,20 +175,39 @@ while True:
         print("You have chosen to add a new contact. ")
         contact_add()
 
+    if user_choice == 2:
+        if len(contacts) == 0:
+            print("There's nothing in your contacts.")
+        else:
+            print("You have chosen to edit a contact. ")
+            contact_edit()
+
     if user_choice == 3:
-        print("You have chosen to delete a contact. ")
+        if len(contacts) == 0:
+            print("There's nothing in your contacts.")
+        else:
+            print("You have chosen to delete a contact. ")
 
     if user_choice == 4:
-        print("You have chosen to search for a contact. ")
-        contact_search()
+        if len(contacts) == 0:
+            print("There's nothing in your contacts.")
+        else:
+            print("You have chosen to search for a contact. ")
+            contact_search()
 
     if user_choice == 5:
-        print("You have chosen to view your contacts. ")
-        contact_display()
+        if len(contacts) == 0:
+            print("There's nothing in your contacts.")
+        else:
+            print("You have chosen to view your contacts. ")
+            contact_display()
 
     if user_choice == 6:
-        print("You have chosen to export your contacts to a text file. ")
-        contact_export()
+        if len(contacts) == 0:
+            print("There's nothing in your contacts.")
+        else:
+            print("You have chosen to export your contacts to a text file. ")
+            contact_export()
 
     if user_choice == 7:
         print("You have chosen to import contacts from a text file. ")
